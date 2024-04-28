cd89ffa6bab0ce3f8ef4cf0f1a2f78340b423d50
Jens Staal <staal1978@gmail.com>
2024-04-28 10:26:35 +0000

    APE cmd directory builds with flex, byacc, gawk etc

---
+++ sys/src/ape/cmd/yacc/config.h
@@ -0,0 +1,17 @@
+/* config.h.  Generated automatically by configure.  */
+/* @configure_input@ */
+/* $Id: config_h.in,v 1.1 1995/01/01 19:34:59 tom Exp $ */
+
+#define SYSTEM_NAME "plan9"
+#define MIXEDCASE_FILENAMES 1
+#define CC_HAS_PROTOS 1
+#define HAVE_FCNTL_H 1
+#define HAVE_ATEXIT 1
+
+/* manually added stuff */
+#define YYPATCH `cat $(srcdir)/VERSION`
+#define _XOPEN_SOURCE 500
+#define _POSIX_C_SOURCE 199506L
+
+
+
