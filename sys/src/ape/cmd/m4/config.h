9f9d0c5e34a6a0034a4ad15b8f4beb7d736db595
Jens Staal <staal1978@gmail.com>
2024-05-07 10:04:11 +0000

    initial attempts at building m4

--- sys/src/ape/cmd/m4/config.h
+++ sys/src/ape/cmd/m4/config.h
@@ -1,2 +1,9 @@
 #include "/sys/include/ape/config.h"
 
+#define ENABLE_CHANGEWORD
+#define UNIX
+
+#define _GL_INLINE_HEADER_BEGIN
+#define _GL_INLINE
+#define _GL_UNUSED
+#define _GL_INLINE_HEADER_END
