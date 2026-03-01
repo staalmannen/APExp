9e75b76fd82ba5fdb0f2eb4d37264b753df44642
Jens Staal <staal1978@gmail.com>
2026-03-01 06:14:55 +0000

    importing the npe libraries was a mistake

--- sys/include/ape/SDL2/SDL_quit.h
+++
@@ -1,6 +0,0 @@
-#ifndef _npe_SDL_quit_h_
-#define _npe_SDL_quit_h_
-
-int SDL_QuitRequested(void);
-
-#endif
--- sys/include/ape/SDL2/SDL_video.h
+++
@@ -1,6 +0,0 @@
-#ifndef _npe_SDL_video_h_
-#define _npe_SDL_video_h_
-
-void SDL_GL_GetDrawableSize(SDL_Window *window, int *w, int *h);
-
-#endif
