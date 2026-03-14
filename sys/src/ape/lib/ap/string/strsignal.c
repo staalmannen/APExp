90a0d17194c873890fe487243db35287f7e7f845
Jens Staal <staal1978@gmail.com>
2026-03-14 09:36:42 +0100

    add some supposedly portable goodies from musl

--- sys/src/ape/lib/ap/string/strsignal.c
+++ sys/src/ape/lib/ap/string/strsignal.c
@@ -2,125 +2,65 @@
 #include <string.h>
 #include "locale_impl.h"
 
-#if (SIGHUP == 1) && (SIGINT == 2) && (SIGQUIT == 3) && (SIGILL == 4) \
- && (SIGTRAP == 5) && (SIGABRT == 6) && (SIGBUS == 7) && (SIGFPE == 8) \
- && (SIGKILL == 9) && (SIGUSR1 == 10) && (SIGSEGV == 11) && (SIGUSR2 == 12) \
- && (SIGPIPE == 13) && (SIGALRM == 14) && (SIGTERM == 15) && (SIGSTKFLT == 16) \
- && (SIGCHLD == 17) && (SIGCONT == 18) && (SIGSTOP == 19) && (SIGTSTP == 20) \
- && (SIGTTIN == 21) && (SIGTTOU == 22) && (SIGURG == 23) && (SIGXCPU == 24) \
- && (SIGXFSZ == 25) && (SIGVTALRM == 26) && (SIGPROF == 27) && (SIGWINCH == 28) \
- && (SIGPOLL == 29) && (SIGPWR == 30) && (SIGSYS == 31)
+/*
+ * APE signal numbers differ substantially from Linux/musl.
+ * APE signal.h defines (from signal.h):
+ *
+ *   SIGHUP   1    SIGINT   2    SIGQUIT  3    SIGILL   4
+ *   SIGABRT  5    SIGFPE   6    SIGKILL  7    SIGSEGV  8
+ *   SIGPIPE  9    SIGALRM  10   SIGTERM  11   SIGUSR1  12
+ *   SIGUSR2  13   SIGBUS   14   SIGCHLD  15   SIGCONT  16
+ *   SIGSTOP  17   SIGTSTP  18   SIGTTIN  19   SIGTTOU  20
+ *   SIGWINCH 21   SIGVTALRM 22  SIGPROF  23
+ *   NSIG     24
+ *
+ * Signals present in musl but absent in APE (no Plan9 equivalent):
+ *   SIGTRAP, SIGURG, SIGXCPU, SIGXFSZ, SIGPWR, SIGSYS,
+ *   SIGPOLL/SIGIO, SIGSTKFLT/SIGEMT, all realtime signals.
+ *
+ * The strings table is indexed 0..NSIG-1 where index 0 is "Unknown signal".
+ * Entry N corresponds to signal number N.
+ */
 
-#define sigmap(x) x
-
-#else
-
-static const char map[] = {
-	[SIGHUP]    = 1,
-	[SIGINT]    = 2,
-	[SIGQUIT]   = 3,
-	[SIGILL]    = 4,
-	[SIGTRAP]   = 5,
-	[SIGABRT]   = 6,
-	[SIGBUS]    = 7,
-	[SIGFPE]    = 8,
-	[SIGKILL]   = 9,
-	[SIGUSR1]   = 10,
-	[SIGSEGV]   = 11,
-	[SIGUSR2]   = 12,
-	[SIGPIPE]   = 13,
-	[SIGALRM]   = 14,
-	[SIGTERM]   = 15,
-#if defined(SIGSTKFLT)
-	[SIGSTKFLT] = 16,
-#elif defined(SIGEMT)
-	[SIGEMT]    = 16,
-#endif
-	[SIGCHLD]   = 17,
-	[SIGCONT]   = 18,
-	[SIGSTOP]   = 19,
-	[SIGTSTP]   = 20,
-	[SIGTTIN]   = 21,
-	[SIGTTOU]   = 22,
-	[SIGURG]    = 23,
-	[SIGXCPU]   = 24,
-	[SIGXFSZ]   = 25,
-	[SIGVTALRM] = 26,
-	[SIGPROF]   = 27,
-	[SIGWINCH]  = 28,
-	[SIGPOLL]   = 29,
-	[SIGPWR]    = 30,
-	[SIGSYS]    = 31
-};
-
-#define sigmap(x) ((x) >= sizeof map ? (x) : map[(x)])
-
-#endif
+/* No sigmap needed: we use APE signal numbers directly as indices.
+ * The table below is laid out to match APE's numbering exactly. */
 
 static const char strings[] =
-	"Unknown signal\0"
-	"Hangup\0"
-	"Interrupt\0"
-	"Quit\0"
-	"Illegal instruction\0"
-	"Trace/breakpoint trap\0"
-	"Aborted\0"
-	"Bus error\0"
-	"Arithmetic exception\0"
-	"Killed\0"
-	"User defined signal 1\0"
-	"Segmentation fault\0"
-	"User defined signal 2\0"
-	"Broken pipe\0"
-	"Alarm clock\0"
-	"Terminated\0"
-#if defined(SIGSTKFLT)
-	"Stack fault\0"
-#elif defined(SIGEMT)
-	"Emulator trap\0"
-#else
-	"Unknown signal\0"
-#endif
-	"Child process status\0"
-	"Continued\0"
-	"Stopped (signal)\0"
-	"Stopped\0"
-	"Stopped (tty input)\0"
-	"Stopped (tty output)\0"
-	"Urgent I/O condition\0"
-	"CPU time limit exceeded\0"
-	"File size limit exceeded\0"
-	"Virtual timer expired\0"
-	"Profiling timer expired\0"
-	"Window changed\0"
-	"I/O possible\0"
-	"Power failure\0"
-	"Bad system call\0"
-	"RT32"
-	"\0RT33\0RT34\0RT35\0RT36\0RT37\0RT38\0RT39\0RT40"
-	"\0RT41\0RT42\0RT43\0RT44\0RT45\0RT46\0RT47\0RT48"
-	"\0RT49\0RT50\0RT51\0RT52\0RT53\0RT54\0RT55\0RT56"
-	"\0RT57\0RT58\0RT59\0RT60\0RT61\0RT62\0RT63\0RT64"
-#if _NSIG > 65
-	"\0RT65\0RT66\0RT67\0RT68\0RT69\0RT70\0RT71\0RT72"
-	"\0RT73\0RT74\0RT75\0RT76\0RT77\0RT78\0RT79\0RT80"
-	"\0RT81\0RT82\0RT83\0RT84\0RT85\0RT86\0RT87\0RT88"
-	"\0RT89\0RT90\0RT91\0RT92\0RT93\0RT94\0RT95\0RT96"
-	"\0RT97\0RT98\0RT99\0RT100\0RT101\0RT102\0RT103\0RT104"
-	"\0RT105\0RT106\0RT107\0RT108\0RT109\0RT110\0RT111\0RT112"
-	"\0RT113\0RT114\0RT115\0RT116\0RT117\0RT118\0RT119\0RT120"
-	"\0RT121\0RT122\0RT123\0RT124\0RT125\0RT126\0RT127\0RT128"
-#endif
+	"Unknown signal\0"      /*  0 - fallback */
+	"Hangup\0"              /*  1 - SIGHUP   */
+	"Interrupt\0"           /*  2 - SIGINT   */
+	"Quit\0"                /*  3 - SIGQUIT  */
+	"Illegal instruction\0" /*  4 - SIGILL   */
+	"Aborted\0"             /*  5 - SIGABRT  */
+	"Arithmetic exception\0"/*  6 - SIGFPE   */
+	"Killed\0"              /*  7 - SIGKILL  */
+	"Segmentation fault\0"  /*  8 - SIGSEGV  */
+	"Broken pipe\0"         /*  9 - SIGPIPE  */
+	"Alarm clock\0"         /* 10 - SIGALRM  */
+	"Terminated\0"          /* 11 - SIGTERM  */
+	"User defined signal 1\0"/* 12 - SIGUSR1 */
+	"User defined signal 2\0"/* 13 - SIGUSR2 */
+	"Bus error\0"           /* 14 - SIGBUS   */
+	"Child process status\0"/* 15 - SIGCHLD  */
+	"Continued\0"           /* 16 - SIGCONT  */
+	"Stopped (signal)\0"    /* 17 - SIGSTOP  */
+	"Stopped\0"             /* 18 - SIGTSTP  */
+	"Stopped (tty input)\0" /* 19 - SIGTTIN  */
+	"Stopped (tty output)\0"/* 20 - SIGTTOU  */
+	"Window changed\0"      /* 21 - SIGWINCH */
+	"Virtual timer expired\0"/* 22 - SIGVTALRM */
+	"Profiling timer expired\0" /* 23 - SIGPROF */
 	"";
 
 char *strsignal(int signum)
 {
 	const char *s = strings;
 
-	signum = sigmap(signum);
-	if (signum - 1U >= _NSIG-1) signum = 0;
+	if (signum < 0 || signum >= NSIG)
+		signum = 0;
 
-	for (; signum--; s++) for (; *s; s++);
+	for (; signum--; s++)
+		for (; *s; s++);
 
 	return (char *)LCTRANS_CUR(s);
 }
