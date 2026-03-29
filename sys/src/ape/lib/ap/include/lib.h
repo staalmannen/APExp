3b91769000c162a14ed58d6c4d795700aca393a7
Jens Staal <staal1978@gmail.com>
2026-03-29 09:15:18 +0200

    libap: move all header files to internal include directory

--- sys/src/ape/lib/ap/include/lib.h
+++ sys/src/ape/lib/ap/include/lib.h
@@ -3,6 +3,43 @@
 #include <fcntl.h>
 #include <ureg.h>
 
+/* pthread lib.h */
+#include <signal.h>
+#include <pthread.h>
+#include "sys9.h"
+
+#define nelem(a)	(sizeof(a)/sizeof((a)[0]))
+
+struct Thread {
+	int		inuse;
+	pid_t		pid;
+
+	Lock		l;
+	int		exited;
+	void		*ret;
+	sigset_t	sigset;
+	int		state;
+};
+typedef struct Thread Thread;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void	_syserrno(void);
+
+extern Thread*	_pthreadalloc(void);
+extern void	_pthreadsetpid(Thread*, pthread_t);
+extern Thread*	_pthreadnew(pthread_t);
+extern Thread*	_pthreadget(pthread_t);
+extern void	_pthreadfree(Thread*);
+
+#ifdef __cplusplus
+}
+#endif
+
+/* end pthread lib.h */
+
 typedef struct Ureg Ureg;
 
 /* mux buf for selecting (see _buf.c) */
