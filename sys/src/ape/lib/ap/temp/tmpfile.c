242e49dc814103086bafc55e0f5811771ce44c24
Jens Staal <staal1978@gmail.com>
2026-04-05 22:26:52 +0200

    fix tmpfile to use musl FILE

--- sys/src/ape/lib/ap/temp/tmpfile.c
+++ sys/src/ape/lib/ap/temp/tmpfile.c
@@ -3,21 +3,18 @@
 #include <string.h>
 #include "sys9.h"
 #undef OPEN
-#include "../include/iolib.h"
+#include <stdio.h>
 #include "lib.h"
 #include "dir.h"
 
 FILE *
 tmpfile(void){
-	FILE *f;
 	static char name[]="/tmp/tf0000000000000";
 	char *p;
 	int n;
-	for(f=_IO_stream;f!=&_IO_stream[FOPEN_MAX];f++)
-		if(f->state==CLOSED)
-			break;
-	if(f==&_IO_stream[FOPEN_MAX])
-		return NULL;
+	int fd;
+
+	/* Find a unique temporary filename */
 	while(access(name, 0) >= 0){
 		p = name+7;
 		while(*p == '9')
@@ -26,19 +23,25 @@ tmpfile(void){
 			return NULL;
 		++*p;
 	}
+
+	/* Create the temporary file with remove-on-close flag */
 	n = _CREATE(name, 64|2, 0777); /* remove-on-close */
 	if(n==-1){
 		_syserrno();
 		return NULL;
 	}
+
 	_fdinfo[n].flags = FD_ISOPEN;
 	_fdinfo[n].oflags = 2;
-	f->fd=n;
-	f->flags=0;
-	f->state=OPEN;
-	f->buf=0;
-	f->rp=0;
-	f->wp=0;
-	f->lp=0;
+
+	/* Use fdopen to create a FILE from the fd */
+	FILE *f = fdopen(n, "w+b");
+	if (!f) {
+		close(n);
+		return NULL;
+	}
+
 	return f;
 }
+
+
