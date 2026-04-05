ad9f0a9a18fc74dfeffbc3409fa3b4b0f5d7a817
Jens Staal <staal1978@gmail.com>
2026-04-05 23:35:02 +0200

    fix __stdio_close and __stdio_seek

--- sys/src/ape/lib/ap/stdio/__stdio_close.c
+++ sys/src/ape/lib/ap/stdio/__stdio_close.c
@@ -1,14 +1,36 @@
 #include "stdio_impl.h"
-#include "aio_impl.h"
-
-static int dummy(int fd)
-{
-	return fd;
-}
-
-weak_alias(dummy, __aio_close);
+#include <unistd.h>
+#include <pthread.h>
+#include <stdint.h>
 
 int __stdio_close(FILE *f)
 {
-	return syscall(SYS_close, __aio_close(f->fd));
+	int ret;
+	pthread_mutex_t *lock;
+
+	if (f->fd < 0) return -1;
+
+	/* Close the file descriptor */
+	ret = close(f->fd);
+	f->fd = -1;
+
+	/* Destroy the pthread mutex if it exists */
+	if (f->lock > 0) {
+		lock = (pthread_mutex_t *)(intptr_t)f->lock;
+		pthread_mutex_destroy(lock);
+		free(lock);
+		f->lock = -1;
+	}
+
+	/* Free any allocated buffer */
+	if (f->flags & F_SVB) {
+		free(f->buf);
+		f->buf = NULL;
+	}
+
+	/* Free the FILE structure itself if it was malloc'd */
+	free(f);
+
+	return ret;
 }
+
--- sys/src/ape/lib/ap/stdio/__stdio_seek.c
+++ sys/src/ape/lib/ap/stdio/__stdio_seek.c
@@ -3,5 +3,19 @@
 
 off_t __stdio_seek(FILE *f, off_t off, int whence)
 {
-	return __lseek(f->fd, off, whence);
+	/* Flush write buffer before seeking */
+	if (f->wpos > f->wbase) {
+		if (write(f->fd, f->wbase, f->wpos - f->wbase) < 0) {
+			return -1;
+		}
+		f->wpos = f->wbase = f->buf;
+		f->wend = f->buf + f->buf_size;
+	}
+
+	/* Clear read buffer */
+	f->rpos = f->rend = 0;
+
+	/* Perform the seek */
+	return lseek(f->fd, off, whence);
 }
+
