ba24ad401312cee3d0ef9218c55634444ddac548
Jens Staal <staal1978@gmail.com>
2026-04-05 22:49:40 +0200

    remove iolib.h and fix rdline for musl stdio

--- sys/src/ape/lib/ap/stdio/rdline.c
+++ sys/src/ape/lib/ap/stdio/rdline.c
@@ -1,68 +1,67 @@
 /*
  * pANS stdio -- rdline
  * This is not a pANS routine.
+ * Adapted for musl stdio
  */
-#include "iolib.h"
+#include "stdio_impl.h"
 #include <string.h>
 
 char *rdline(FILE *f, char **ep){
 	int cnt;
 	char *nlp, *vp;
-	switch(f->state){
-	default:	/* CLOSED, WR, ERR, EOF */
+
+	/* Ensure we're in read mode */
+	if (__toread(f)) {
 		return NULL;
-	case OPEN:
-		if(_IO_setvbuf(f)!=0)
-			return NULL;
-	case RDWR:
-		f->state=RD;
-	case RD:
-		if(f->bufl==0){		/* Called by a comedian! */
-			f->state=ERR;
-			return NULL;
-		}
-		vp=f->rp;
-		for(;;){
-			/*
-			 * Look for a newline.
-			 * If none found, slide the partial line to the beginning
-			 * of the buffer, read some more and keep looking.
-			 */
-			nlp=memchr(f->rp, '\n', f->wp-f->rp);
-			if(nlp!=0) break;
-			if(f->flags&STRING){
-				f->rp=f->wp;
-				if(ep) *ep=f->wp;
-				return vp;
-			}
-			if(f->rp!=f->buf){
-				memmove(f->buf, f->rp, f->wp-f->rp);
-				f->wp-=f->rp-f->buf;
-				f->rp=f->buf;
-				vp=f->rp;
-			}
-			cnt=f->bufl-(f->wp-f->buf);
-			if(cnt==0){	/* no room left */
-				nlp=f->wp-1;
-				break;
-			}
-			cnt=read(f->fd, f->wp, cnt);
-			if(f->state==CLOSED)
-				return NULL;
-			if(cnt==-1){
-				f->state=ERR;
-				return NULL;
-			}
-			if(cnt==0){	/* is this ok? */
-				f->state=EOF;
-				return NULL;
-			}
-			f->rp=f->wp;
-			f->wp+=cnt;
-		}
-		*nlp='\0';
-		f->rp=nlp+1;
-		if(ep) *ep=nlp;
-		return vp;
 	}
+
+	/* Initialize buffer if needed */
+	if (!f->buf || f->buf_size == 0) {
+		if (setvbuf(f, NULL, _IOFBF, BUFSIZ) != 0) {
+			return NULL;
+		}
+	}
+
+	vp = f->rpos;
+	for(;;){
+		/*
+		 * Look for a newline.
+		 * If none found, slide the partial line to the beginning
+		 * of the buffer, read some more and keep looking.
+		 */
+		nlp = memchr(f->rpos, '\n', f->rend - f->rpos);
+		if(nlp != 0) break;
+
+		if(f->rpos != f->buf){
+			memmove(f->buf, f->rpos, f->rend - f->rpos);
+			f->rend -= f->rpos - f->buf;
+			f->rpos = f->buf;
+			vp = f->rpos;
+		}
+
+		cnt = f->buf_size - (f->rend - f->buf);
+		if(cnt == 0){	/* no room left */
+			nlp = f->rend - 1;
+			break;
+		}
+
+		cnt = read(f->fd, f->rend, cnt);
+		if(cnt == -1){
+			f->flags |= F_ERR;
+			return NULL;
+		}
+		if(cnt == 0){
+			f->flags |= F_EOF;
+			return NULL;
+		}
+
+		f->rpos = f->rend;
+		f->rend += cnt;
+	}
+
+	*nlp = '\0';
+	f->rpos = nlp + 1;
+	if(ep) *ep = nlp;
+	return vp;
 }
+
