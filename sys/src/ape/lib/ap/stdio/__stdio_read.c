5f061008fdec7e7f7f79e6e0b993df27da7ddd79
Jens Staal <staal1978@gmail.com>
2026-04-05 23:26:17 +0200

    fix __stdio_read

--- sys/src/ape/lib/ap/stdio/__stdio_read.c
+++ sys/src/ape/lib/ap/stdio/__stdio_read.c
@@ -1,24 +1,23 @@
 #include "stdio_impl.h"
-#include <sys/uio.h>
+#include <unistd.h>
+#include <errno.h>
 
 size_t __stdio_read(FILE *f, unsigned char *buf, size_t len)
 {
-	struct iovec iov[2] = {
-		{ .iov_base = buf, .iov_len = len - !!f->buf_size },
-		{ .iov_base = f->buf, .iov_len = f->buf_size }
-	};
 	ssize_t cnt;
 
-	cnt = iov[0].iov_len ? syscall(SYS_readv, f->fd, iov, 2)
-		: syscall(SYS_read, f->fd, iov[1].iov_base, iov[1].iov_len);
+	/* Simple approach: just read directly into the provided buffer */
+	cnt = read(f->fd, buf, len);
+
 	if (cnt <= 0) {
-		f->flags |= cnt ? F_ERR : F_EOF;
+		if (cnt == 0) {
+			f->flags |= F_EOF;
+		} else {
+			f->flags |= F_ERR;
+		}
 		return 0;
 	}
-	if (cnt <= iov[0].iov_len) return cnt;
-	cnt -= iov[0].iov_len;
-	f->rpos = f->buf;
-	f->rend = f->buf + cnt;
-	if (f->buf_size) buf[len-1] = *f->rpos++;
-	return len;
+
+	return (size_t)cnt;
 }
+
