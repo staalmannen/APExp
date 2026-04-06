767c855c50b2c76193a8779afed027a4a9c32651
Jens Staal <staal1978@gmail.com>
2026-04-06 07:55:12 +0200

    fixing the final remaining issues for stdio test?

--- sys/src/ape/lib/ap/stdio/__stdio_exit.c
+++ sys/src/ape/lib/ap/stdio/__stdio_exit.c
@@ -1,25 +1,40 @@
 #include "stdio_impl.h"
+#include <stdio.h>
 
-static FILE *volatile dummy_file = 0;
-weak_alias(dummy_file, __stdin_used);
-weak_alias(dummy_file, __stdout_used);
-weak_alias(dummy_file, __stderr_used);
+/* APExp: Replace musl weak_alias with proper extern declarations */
+FILE *volatile __stdin_used = NULL;
+FILE *volatile __stdout_used = NULL;
+FILE *volatile __stderr_used = NULL;
 
 static void close_file(FILE *f)
 {
 	if (!f) return;
 	FFINALLOCK(f);
-	if (f->wpos != f->wbase) f->write(f, 0, 0);
-	if (f->rpos != f->rend) f->seek(f, f->rpos-f->rend, SEEK_CUR);
+	if (f->wpos != f->wbase) f->write(f, (unsigned char *)f->wpos - (unsigned char *)f->wbase, 0);
+	if (f->rpos != f->rend) f->seek(f, f->rpos - f->rend, SEEK_CUR);
 }
 
 void __stdio_exit(void)
 {
 	FILE *f;
-	for (f=*__ofl_lock(); f; f=f->next) close_file(f);
+	FILE **head = __ofl_lock();
+
+	/* Close all files in open file list */
+	for (f = *head; f; f = f->next) {
+		close_file(f);
+	}
+
+	__ofl_unlock();
+
+	/* Close standard streams if they exist */
 	close_file(__stdin_used);
 	close_file(__stdout_used);
 	close_file(__stderr_used);
 }
 
-weak_alias(__stdio_exit, __stdio_exit_needed);
+/* Alias for compatibility */
+void __stdio_exit_needed(void)
+{
+	__stdio_exit();
+}
+
