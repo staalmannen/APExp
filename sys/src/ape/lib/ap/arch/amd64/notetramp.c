f6855b1e29c716422947c812ab2bdb31271e727b
Jens Staal <staal1978@gmail.com>
2026-04-17 07:29:38 +0200

    simplifications of notetramp and alignments with sigsetjmp

--- sys/src/ape/lib/ap/arch/amd64/notetramp.c
+++ sys/src/ape/lib/ap/arch/amd64/notetramp.c
@@ -16,11 +16,6 @@ static int nstack = 0;
 
 static void notecont(Ureg*, char*);
 
-/*
- * _notetramp is called by the signal dispatcher.
- * The standard Plan 9 signature is (Ureg *u, char *msg).
- * APE's signal.c wraps this to provide (int sig, void(*hdlr)(int, char*, Ureg*), Ureg *u).
- */
 void
 _notetramp(int sig, void (*hdlr)(int, char*, Ureg*), Ureg *u)
 {
@@ -70,20 +65,22 @@ siglongjmp(sigjmp_buf j, int ret)
 		_psigblocked = jb->blocked;
 	}
 
-	/* 
-	 * If we're not in a signal handler, or jumping to a frame that was 
-	 * active AFTER the signal, use normal longjmp.
-	 */
-	if(nstack == 0 || pcstack[nstack-1].u->sp > jb->jmpbuf[0]){
+	if(nstack == 0){
 		longjmp((void*)jb->jmpbuf, ret);
 	}
 
+	/* Always use NRSTR if in a signal handler to clear kernel state */
 	u = pcstack[nstack-1].u;
 	nstack--;
 
 	u->ax = (ret == 0) ? 1 : ret;
 	u->pc = jb->jmpbuf[1];
-	u->sp = jb->jmpbuf[0];
+	/* 
+	 * Restoring SP to the state it was at the entry of sigsetjmp.
+	 * Since sigsetjmp was a function call, SP points to the return address.
+	 * We add 8 to simulate a RET having occurred.
+	 */
+	u->sp = jb->jmpbuf[0] + 8;
 	_NOTED(3);	/* NRSTR */
 	_EXITS("siglongjmp failed");
 }
