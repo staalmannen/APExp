8b59a8bac20edb8dd50680e7852e4fe3de572648
Jens Staal <staal1978@gmail.com>
2026-05-01 13:54:58 +0200

    add the c32 ctypes as shims to libutf too

--- sys/include/ape/uchar.h
+++ sys/include/ape/uchar.h
@@ -6,6 +6,19 @@
 
 typedef uint32_t char32_t;
 
+int c32isalnum(char32_t r);
+int c32isalpha(char32_t r);
+int c32isblank(char32_t r);
+int c32iscntrl(char32_t r);
+int c32isdigit(char32_t r);
+int c32isgraph(char32_t r);
+int c32islower(char32_t r);
+int c32isprint(char32_t r);
+int c32ispunct(char32_t r);
+int c32isspace(char32_t r);
+int c32isupper(char32_t r);
+int c32isxdigit(char32_t r);
+
 ucs4_t c32tolower(ucs4_t r);
 ucs4_t c32toupper(ucs4_t r);
 size_t c32rtomb(char *s, ucs4_t wc, mbstate_t *ps);
---
+++ sys/src/ape/lib/ap/multibyte/c32-ctype.c
@@ -0,0 +1,15 @@
+#include <uchar.h>
+#include <utf.h>
+
+int c32isalnum(char32_t r) { return isalnumrune(r); }
+int c32isalpha(char32_t r) { return isalpharune(r); }
+int c32isblank(char32_t r) { return isblankrune(r); }
+int c32iscntrl(char32_t r) { return iscntrlrune(r); }
+int c32isdigit(char32_t r) { return isdigitrune(r); }
+int c32isgraph(char32_t r) { return isgraphrune(r); }
+int c32islower(char32_t r) { return islowerrune(r); }
+int c32isprint(char32_t r) { return isprintrune(r); }
+int c32ispunct(char32_t r) { return ispunctrune(r); }
+int c32isspace(char32_t r) { return isspacerune(r); }
+int c32isupper(char32_t r) { return isupperrune(r); }
+int c32isxdigit(char32_t r) { return isxdigitrune(r); }
