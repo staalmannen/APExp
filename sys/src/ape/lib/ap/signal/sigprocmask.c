0759fdf5dc36aeb7c691dc0b942a2ebfaf3e0806
Jens Staal <staal1978@gmail.com>
2026-05-05 15:53:05 +0200

    acid debugging of the crashing bash binary indicated a bug in sigprocmask

--- sys/src/ape/lib/ap/signal/sigprocmask.c
+++ sys/src/ape/lib/ap/signal/sigprocmask.c
@@ -9,15 +9,17 @@ sigprocmask(int how, sigset_t *set, sigset_t *oset)
 {
 	if(oset)
 		*oset = _psigblocked;
-	if(how==SIG_BLOCK)
-		_psigblocked |= *set;
-	else if(how==SIG_UNBLOCK)
-		_psigblocked &= ~*set;
-	else if(how==SIG_SETMASK)
-		_psigblocked = *set;
-	else{
-		errno = EINVAL;
-		return -1;
+	if(set) {
+		if(how==SIG_BLOCK)
+			_psigblocked |= *set;
+		else if(how==SIG_UNBLOCK)
+			_psigblocked &= ~*set;
+		else if(how==SIG_SETMASK)
+			_psigblocked = *set;
+		else{
+			errno = EINVAL;
+			return -1;
+		}
 	}
 	return 0;
 }
