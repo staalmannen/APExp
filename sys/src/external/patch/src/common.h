cbcf62261cc370eecfa0857a7ced948c67d510ab
Jens Staal <staal1978@gmail.com>
2026-05-18 11:42:23 +0200

    bool issues in patch merge.c

--- sys/src/external/patch/src/common.h
+++ sys/src/external/patch/src/common.h
@@ -50,6 +50,13 @@
 #define ARRAY_SIZE(a) (sizeof (a) / sizeof ((a)[0]))
 
 /* typedefs */
+typedef int bool;
+#ifndef true
+#define true 1
+#endif
+#ifndef false
+#define false 0
+#endif
 
 /* A description of an output file.  It may be temporary.  */
 struct outfile
