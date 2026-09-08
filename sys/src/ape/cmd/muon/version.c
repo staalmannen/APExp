3bedd0f2f26e02c0c153c4cb0a275d53ce10e159
Jens Staal <staal1978@gmail.com>
2026-09-08 11:01:32 +0200

    initial attempt at a mkfile for muon

---
+++ sys/src/ape/cmd/muon/version.c
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: Stone Tickle <lattis@mochiro.moe>
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+#include "compat.h"
+
+#include "version.h"
+
+const struct muon_version muon_version = {
+	.version = "0.5.0",
+	.vcs_tag = "",
+	.meson_compat = "1.7.99",
+};
